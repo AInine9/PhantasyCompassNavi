d5328aa1540d7029f014bde3bc74e244b597d838
hugu1026 <hugu1026@gmail.com>
2018-04-07 12:19:23 +0900

    [add]目的地ユーティリティを追加

---
+++ src/main/java/hugu1026/com/github/phantasycompassnavi/util/DestinationUtil.java
@@ -0,0 +1,100 @@
+package hugu1026.com.github.phantasycompassnavi.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+
+public class DestinationUtil {
+
+    public static void createDestinationYml() {
+        File fileData = new File(Bukkit.getServer().getPluginManager().getPlugin("PhantasyCompassNavi").getDataFolder(), File.separator + "destination");
+        File file = new File(fileData, File.separator + "destination.yml");
+        FileConfiguration destinationData = YamlConfiguration.loadConfiguration(file);
+
+        if (!file.exists()) {
+            try {
+                destinationData.save(file);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static File getDestinationFile() {
+        File fileData = new File(Bukkit.getServer().getPluginManager().getPlugin("PhantasyCompassNavi").getDataFolder(), File.separator + "destination");
+        return new File(fileData, File.separator + "destination.yml");
+    }
+
+    public static FileConfiguration getDestinationData() {
+        File file = getDestinationFile();
+        return YamlConfiguration.loadConfiguration(file);
+    }
+
+    public static void saveDestinationData(File file, FileConfiguration data) {
+        try {
+            data.save(file);
+        } catch (IOException ex) {
+            Bukkit.getServer().getLogger().severe("データの保存に失敗しました");
+            ex.printStackTrace();
+        }
+    }
+
+    public static void createSection(String name) {
+        File file = getDestinationFile();
+        FileConfiguration destinationData = getDestinationData();
+
+        destinationData.createSection(name);
+        destinationData.createSection(name + ".location");
+        destinationData.createSection(name + ".item");
+
+        saveDestinationData(file, destinationData);
+    }
+
+    public static void setLocation(String name, Location location) {
+        File destinationFile = getDestinationFile();
+        FileConfiguration destinationData = getDestinationData();
+
+        destinationData.set(name + ".location", location);
+        saveDestinationData(destinationFile, destinationData);
+    }
+
+    public static void setItem(String name, int id) {
+        File destinationFile = getDestinationFile();
+        FileConfiguration destinationData = getDestinationData();
+
+        destinationData.set(name + ".item", id);
+        saveDestinationData(destinationFile, destinationData);
+    }
+
+    public static Location getLocation(String name) {
+        FileConfiguration destinationData = getDestinationData();
+
+        return (Location) destinationData.get(name + ".location");
+    }
+
+    public static ItemStack getItemStack(String name) {
+        FileConfiguration destinationData = getDestinationData();
+
+        ItemStack itemStack = new ItemStack(Material.getMaterial((Integer) destinationData.get(name + ".item")), 1);
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.setDisplayName(ChatColor.GOLD + name);
+        itemStack.setItemMeta(meta);
+
+        return itemStack;
+    }
+
+    public static HashSet<String> getNameList() {
+        FileConfiguration destinationData = getDestinationData();
+
+        return new HashSet<>(destinationData.getKeys(false));
+    }
+}
