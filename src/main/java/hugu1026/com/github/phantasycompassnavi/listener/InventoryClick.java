d6de291c8049530ee67efe65d2f6775e9191251a
hugu1026 <hugu1026@gmail.com>
2018-04-07 23:42:51 +0900

    [add]テレポートが出来るように

--- src/main/java/hugu1026/com/github/phantasycompassnavi/listener/InventoryClick.java
+++ src/main/java/hugu1026/com/github/phantasycompassnavi/listener/InventoryClick.java
@@ -1,7 +1,10 @@
 package hugu1026.com.github.phantasycompassnavi.listener;
 
+import hugu1026.com.github.phantasycompassnavi.PhantasyCompassNavi;
 import hugu1026.com.github.phantasycompassnavi.gui.CompassGui;
+import hugu1026.com.github.phantasycompassnavi.gui.TeleportGui;
 import hugu1026.com.github.phantasycompassnavi.util.DestinationUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -10,6 +13,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class InventoryClick implements Listener {
 
@@ -17,8 +22,6 @@ public class InventoryClick implements Listener {
     public void InventoryClick(InventoryClickEvent event) {
         Inventory inventory = event.getInventory();
         if (inventory == null) return;
-
-        if (!(inventory.getHolder() instanceof CompassGui)) return;
 
         if (!(event.getWhoClicked() instanceof Player)) return;
 
@@ -30,14 +33,25 @@ public class InventoryClick implements Listener {
         String name = clickedItem.getItemMeta().getDisplayName();
         name = ChatColor.stripColor(name);
 
-        player.setCompassTarget(DestinationUtil.getLocation(name));
-        player.sendMessage(ChatColor.RED + name + ChatColor.GOLD + "をナビゲーターの目標地点に設定しました");
+        if (inventory.getHolder() instanceof CompassGui) {
+            player.setCompassTarget(DestinationUtil.getLocation(name));
+            player.sendMessage(ChatColor.RED + name + ChatColor.GOLD + "をナビゲーターの目標地点に設定しました");
 
-        Location destinationLoc = DestinationUtil.getLocation(name);
-        Location playerLoc = player.getLocation();
+            Location destinationLoc = DestinationUtil.getLocation(name);
+            Location playerLoc = player.getLocation();
 
-        player.sendMessage(ChatColor.GOLD + "目的地地点までおよそ:" + ChatColor.RED + (int) destinationLoc.distance(playerLoc) + ChatColor.GOLD + "ブロック");
+            player.sendMessage(ChatColor.GOLD + "目的地地点までおよそ:" + ChatColor.RED + (int) destinationLoc.distance(playerLoc) + ChatColor.GOLD + "ブロック");
+        }
 
+        else if (inventory.getHolder() instanceof TeleportGui) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20 * 3, 100));
+            player.sendMessage(ChatColor.GOLD + "3秒後にテレポートします...");
+            String finalName = name;
+            Bukkit.getScheduler().scheduleSyncDelayedTask(PhantasyCompassNavi.getPlugin(PhantasyCompassNavi.class), () -> {
+                player.teleport(DestinationUtil.getLocation(finalName));
+                player.sendMessage(ChatColor.RED + finalName + ChatColor.GOLD + "にテレポートしました");
+            }, 60);
+        }
         player.closeInventory();
         event.setCancelled(true);
     }
