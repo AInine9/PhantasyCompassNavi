a6cdfb99a08e2c03f235a2375d3d5adde5bf05f3
hugu1026 <hugu1026@gmail.com>
2018-04-07 15:34:26 +0900

    [fix]コマンドの引数不足エラー

--- src/main/java/hugu1026/com/github/phantasycompassnavi/command/SetDestinationCommand.java
+++ src/main/java/hugu1026/com/github/phantasycompassnavi/command/SetDestinationCommand.java
@@ -2,6 +2,7 @@ package hugu1026.com.github.phantasycompassnavi.command;
 
 import hugu1026.com.github.phantasycompassnavi.PhantasyCompassNavi;
 import hugu1026.com.github.phantasycompassnavi.util.DestinationUtil;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -18,15 +19,25 @@ public class SetDestinationCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("destination")) {
-            if (args[0].equalsIgnoreCase("set")) {
-                if (sender instanceof Player) {
-                    Player player = (Player) sender;
-                    if (args[1] != null || args[2] != null) {
-                        DestinationUtil.createSection(args[1]);
-                        DestinationUtil.setLocation(args[1], player.getLocation());
-                        DestinationUtil.setItem(args[1], Integer.parseInt(args[2]));
-                        player.sendMessage(ChatColor.GOLD + args[1] + "を設定しました");
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (args.length == 3) {
+                    if (args[0].equalsIgnoreCase("set")) {
+                        if (args[1] != null || args[2] != null) {
+                            if (StringUtils.isNumeric(args[2])) {
+                                DestinationUtil.createSection(args[1]);
+                                DestinationUtil.setLocation(args[1], player.getLocation());
+                                DestinationUtil.setItem(args[1], Integer.parseInt(args[2]));
+                                player.sendMessage(ChatColor.GOLD + args[1] + "を設定しました");
+                            } else {
+                                player.sendMessage(ChatColor.RED + "アイテムIDは自然数で指定してください");
+                                return false;
+                            }
+                        }
                     }
+                } else {
+                    player.sendMessage(ChatColor.RED + "/destination set <目的地名> <アイテムID>");
+                    return false;
                 }
             }
         }
